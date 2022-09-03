2ecc265a19e7020eeeb98155e5949e35c8dd884f
aaron2494 <aaron.e.francolino@gmail.com>
2022-09-03 14:12:37 -0300

    1er commit

---
+++ src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Component} from '@angular/core';
+import { Personaje } from 'src/app/interfaces/Personaje';
+
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent  {
+personajes :Personaje[]|undefined;
+personajesCopy:Personaje[]|undefined
+
+
+  constructor( public http:HttpClient ) { 
+    this.getData();
+  }
+
+  async getData(){
+    await this.http
+    .get<any>(environment.apiUrl + "/characters")
+    .subscribe((res)=>{
+
+      this.personajes= res.map(({char_id,name,nickname,img,status,occupation}:Personaje)=>{
+        return{
+          char_id:char_id,
+          name:name,
+          nickname:nickname,
+          img:img,
+          status:status,
+          occupation:occupation,
+        };
+      });
+      this.personajesCopy=this.personajes
+     
+    });
+    
+  }
+
+
+  filter(e:any){
+    const search:string =e.target.value;
+   
+    this.personajes=this.personajesCopy?.filter(({name}:Personaje)=>{
+      return name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    });
+  }
+}
+
+
